c357fce35620af64c14795d4d602b91b9739b96f
Vivek <dev.vivekkhanal@gmail.com>
2021-07-31 18:15:52 +0530

    partially fixed sidebar visibility

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log(1 + 1);
 const navSlide = () => {
   const menu = document.querySelector('.menu');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  
 
   menu.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
